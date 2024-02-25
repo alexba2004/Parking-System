ccabb8a821807e57ddead2f97ddc2072002141a0
Briones <j.alejandro.briones.a22@gmail.com>
2024-02-25 13:45:04 -0600

    delete: files

--- reportes.php
+++
@@ -1,7 +0,0 @@
-<div class="row mt-2 px-5">
-    <div class="b-4">
-        <div class="card">
-            <div class="card-body text-center" >
-                <p class="font-weight-bold">Solo reportes</p>
-        </div>
-    </div>
